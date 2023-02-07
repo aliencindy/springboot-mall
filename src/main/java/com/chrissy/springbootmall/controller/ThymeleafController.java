a7db558a1796ddd323cb25fb996e84822808c315
Chrissy <lucy8611892@gmail.com>
2023-02-07 16:25:31 +0800

    接上前端 - Login、Register

--- src/main/java/com/chrissy/springbootmall/controller/ThymeleafController.java
+++ src/main/java/com/chrissy/springbootmall/controller/ThymeleafController.java
@@ -1,7 +1,12 @@
 package com.chrissy.springbootmall.controller;
 
+import com.chrissy.springbootmall.dto.UserLoginRequest;
+import com.chrissy.springbootmall.dto.UserRegisterRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ThymeleafController {
@@ -12,12 +17,22 @@ public class ThymeleafController {
     }
 
     @GetMapping("/login") // 跳轉到登入畫面
-    public String login(){
+    public String login(Model model){
+        UserLoginRequest userLoginRequest = new UserLoginRequest();
+        model.addAttribute("userLoginRequest",userLoginRequest);// （變數名稱，變數值)
         return "login";
     }
 
+//    @PostMapping("/submit")
+//    public String add(@ModelAttribute UserLoginRequest userLoginRequest, Model model) {
+//        model.addAttribute("userLoginRequest", userLoginRequest);
+//        return "submit";
+//    }
+
     @GetMapping("/register") // 跳轉到註冊畫面
-    public String register(){
+    public String register(Model model){
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        model.addAttribute("userRegisterRequest",userRegisterRequest);
         return "register";
     }
 
--- src/main/java/com/chrissy/springbootmall/controller/UserController.java
+++ src/main/java/com/chrissy/springbootmall/controller/UserController.java
@@ -4,11 +4,15 @@ import com.chrissy.springbootmall.dto.UserLoginRequest;
 import com.chrissy.springbootmall.dto.UserRegisterRequest;
 import com.chrissy.springbootmall.model.User;
 import com.chrissy.springbootmall.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
@@ -20,7 +24,7 @@ public class UserController {
     private UserService userService;
 
 
-//    @PostMapping("/users/register")
+//    @PostMapping("/users/register")//By teacher
 //    public ResponseEntity<User> register(@RequestBody @Valid UserRegisterRequest userRegisterRequest) {
 //        Integer userId = userService.register(userRegisterRequest);
 //        User user = userService.getUserById(userId);
@@ -28,7 +32,7 @@ public class UserController {
 //        return ResponseEntity.status(HttpStatus.CREATED).body(user);
 //    }
 //
-//    @PostMapping("/users/login")
+//    @PostMapping("/users/login")//By teacher
 //    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest) {
 //        User user = userService.login(userLoginRequest);
 //
@@ -36,29 +40,46 @@ public class UserController {
 //
 //    }
     @PostMapping("/users/register")
-    public ResponseEntity<User> register(@RequestParam String email, @RequestParam String password) {
-        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
-        userRegisterRequest.setEmail(email);
-        userRegisterRequest.setPassword(password);
+    public ModelAndView register(@ModelAttribute UserRegisterRequest userRegisterRequest,Model model) throws JsonProcessingException {
+        model.addAttribute("userRegisterRequest",userRegisterRequest);
 
         Integer userId = userService.register(userRegisterRequest);
         User user = userService.getUserById(userId);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResult = objectMapper.writeValueAsString(user);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+        model.addAttribute("msg", jsonResult);
+        return new ModelAndView("/submit");
     }
 
-    @PostMapping("/users/login")
-    public ResponseEntity<User> login(@RequestParam String email, @RequestParam String password) {
-        UserLoginRequest userLoginRequest = new UserLoginRequest();
-        userLoginRequest.setEmail(email);
-        userLoginRequest.setPassword(password);
+//    @PostMapping("/users/login") // OK
+
+//    public ResponseEntity<User> login(@RequestParam String email,
+//                                      @RequestParam String password) {
+//        UserLoginRequest userLoginRequest = new UserLoginRequest();
+//        userLoginRequest.setEmail(email);
+//        userLoginRequest.setPassword(password);
+//
+//        User user = userService.login(userLoginRequest);
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(user);
+//
+//    }
+
+    @PostMapping("/users/login")// OK
+    public ModelAndView login(@ModelAttribute UserLoginRequest userLoginRequest, Model model) throws JsonProcessingException {
+        model.addAttribute("userLoginRequest", userLoginRequest);
 
         User user = userService.login(userLoginRequest);
-
-        return ResponseEntity.status(HttpStatus.OK).body(user);
-
+        if(user==null){
+            model.addAttribute("msg", "hi");
+        }else{
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonResult = objectMapper.writeValueAsString(user);
+            model.addAttribute("msg",jsonResult);
+        }
+        return new ModelAndView("/submit");
     }
-
 
 }
 
